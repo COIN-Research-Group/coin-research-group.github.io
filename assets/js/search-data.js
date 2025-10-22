9cbb3e5f7bcebf6e25fae23735b407949c1c1998
jforsyth <forsy2jb@jmu.edu>
2025-10-22 19:49:49 +0000

    Deploying to gh-pages from @ COIN-Research-Group/coin-research-group.github.io@2d48f5ef00468a3d99ac648fafb85744bc718779 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "Active projects in the COIN Research Group",
+          description: "Active projects in the COIN Research Group and the Madison Art Collection",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
@@ -49,57 +49,32 @@ ninja.data = [{
             },},{id: "news-site-launch",
           title: 'Site Launch!',
           description: "",
-          section: "News",},{id: "projects-provenance-recovery",
-          title: 'Provenance Recovery',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_provenance/";
-            },},{id: "projects-coin-attribution",
-          title: 'Coin Attribution',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_attribution/";
-            },},{id: "projects-3d-modeling-and-digital-twins",
+          section: "News",},{id: "projects-3d-modeling-and-digital-twins",
           title: '3D Modeling and Digital Twins',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_modeling/";
+            },},{id: "projects-project-4",
+          title: 'project 4',
+          description: "another without an image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/4_scanning/";
+            },},{id: "projects-project-5",
+          title: 'project 5',
+          description: "a project with a background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_tracking/";
+            },},{id: "projects-coin-attribution",
+          title: 'Coin Attribution',
+          description: "Updating the Sawhill Collection with modern numismatic references",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/attribution/";
+            },},{id: "projects-provenance-recovery",
+          title: 'Provenance Recovery',
+          description: "Finding the origins of the Sawhill Collection",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/provenance_recovery/";
             },},{
-        id: 'social-email',
-        title: 'email',
-        section: 'Socials',
-        handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
